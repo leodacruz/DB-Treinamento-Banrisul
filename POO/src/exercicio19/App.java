00e9537b2c94e2f107365b7805f02c155ff3b840
leodacruz <72158477+leodacruz@users.noreply.github.com>
2023-02-01 11:29:18 -0300

    Add files via upload

---
+++ POO/src/exercicio19/App.java
@@ -0,0 +1,13 @@
+/*Implemente um sistema de agenda telefonica. Faça (ao menos)
+as interacoes de adicionar e deletar numeros.*/
+
+package exercicio19;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
